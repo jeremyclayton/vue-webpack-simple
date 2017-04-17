fa8eb31635f40c56762a90de51945e8b19597521
jeremyclayton <jeremy.clayton.91@gmail.com>
2017-04-17 11:49:43 -0600

    added vue router

---
+++ reads/src/components/Book.vue
@@ -0,0 +1,26 @@
+<template>
+<div id="app">
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <h3>Header</h3>
+        </div>
+    </nav>
+    <ul>
+        <li>Anything</li>
+    </ul>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'app',
+
+}
+</script>
+
+<style lang="scss">
+body {
+    padding-top: 70px;
+}
+
+</style>
--- reads/src/main.js
+++ reads/src/main.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
-import Routes from './routes.js'
+import routes from './routes.js'
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
-  Routes,
+  routes,
   mode: 'history'
 });
 
